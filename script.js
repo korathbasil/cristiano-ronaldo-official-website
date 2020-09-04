a80a20210dc8a67083a27f17a596797096f6a478
korathbasil <korathbasil@live.com>
2020-09-05 00:20:17 +0530

    hero finished

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const videCard = document.getElementsByClassName(hero__videoCard);
+const videCard = document.querySelector(".hero__videoCard");
 
-const imageCard = document.getElementsByClassName(hero__imageCard);
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+const img4 = document.getElementById("img4");
+const img5 = document.getElementById("img5");
 
-imageCard.addEventListener("mouseover", () => (videCard.style.width = "15%"));
-imageCard.addEventListener("mouseleave", () => (videCard.style.width = "25%"));
+img1.addEventListener("mouseover", () => (videCard.style.width = "15%"));
+img1.addEventListener("mouseleave", () => (videCard.style.width = "25%"));
+
+img2.addEventListener("mouseover", () => (videCard.style.width = "15%"));
+img2.addEventListener("mouseleave", () => (videCard.style.width = "25%"));
+
+img3.addEventListener("mouseover", () => (videCard.style.width = "15%"));
+img3.addEventListener("mouseleave", () => (videCard.style.width = "25%"));
+
+img4.addEventListener("mouseover", () => (videCard.style.width = "15%"));
+img4.addEventListener("mouseleave", () => (videCard.style.width = "25%"));
+
+img5.addEventListener("mouseover", () => (videCard.style.width = "15%"));
+img5.addEventListener("mouseleave", () => (videCard.style.width = "25%"));
